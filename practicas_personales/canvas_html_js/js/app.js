4442b7a629068468a8d4bd74bcf6922299bc5ff1
Gonzalotd <54744142+Gonzalotd@users.noreply.github.com>
2025-09-27 14:05:02 +0200

    Entrenamiento

---
+++ practicas_personales/canvas_html_js/js/app.js
@@ -0,0 +1,18 @@
+(() => {
+   
+    const draw = () => {
+        const canvas = document.querySelector('#canvas');
+
+        if ( canvas.getContext ) {
+            const ctx = canvas.getContext("2d");
+
+            ctx.fillStyle = "rgb(200 0 0)";
+            ctx.fillRect(10, 10, 50, 50);
+
+            ctx.fillStyle = "rgb(0 0 200 / 50%)";
+            ctx.fillRect(30, 30, 50, 50);
+        }
+    }
+
+    draw();
+})();
